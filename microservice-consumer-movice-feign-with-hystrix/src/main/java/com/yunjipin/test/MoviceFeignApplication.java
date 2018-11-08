f6d24915bde26f3b0bc711f5a94d936e724fd019
674145395@qq.com <674145395@qq.com>
2018-11-08 14:33:35 +0800

    hytrix对feign的支持，不同的cloud版本写法有区别

---
+++ microservice-consumer-movice-feign-with-hystrix/src/main/java/com/yunjipin/test/MoviceFeignApplication.java
@@ -0,0 +1,20 @@
+package com.yunjipin.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableHystrix
+public class MoviceFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviceFeignApplication.class, args);
+	}
+}
---
+++ microservice-consumer-movice-feign-with-hystrix/src/main/java/com/yunjipin/test/controller/MovieFeignController.java
@@ -0,0 +1,24 @@
+package com.yunjipin.test.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yunjipin.test.entity.User;
+import com.yunjipin.test.feign.UserFeignClient;
+
+@RestController
+public class MovieFeignController {
+
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	@GetMapping("/movie/{id}")
+	public User findById(@PathVariable Long id) {
+		return userFeignClient.findById(id);
+// 	   return this.restTemplate.getForObject(this.userServicePath + id, User.class);
+	}
+	
+}
