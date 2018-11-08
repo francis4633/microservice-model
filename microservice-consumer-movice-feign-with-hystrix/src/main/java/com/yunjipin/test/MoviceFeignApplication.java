cba2fabf044b02237ec0500022653f64a8abf9f0
674145395@qq.com <674145395@qq.com>
2018-11-08 16:11:13 +0800

    @SpringCloudApplication的使用，他包含了/@EnableHystrix以及@SpringBootApplication，但仍需要开启feign对hystrix的支持

--- microservice-consumer-movice-feign-with-hystrix/src/main/java/com/yunjipin/test/MoviceFeignApplication.java
+++ microservice-consumer-movice-feign-with-hystrix/src/main/java/com/yunjipin/test/MoviceFeignApplication.java
@@ -3,15 +3,17 @@ package com.yunjipin.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-@SpringBootApplication
-@EnableEurekaClient
+//@SpringBootApplication
+@SpringCloudApplication
+//@EnableEurekaClient
 @EnableFeignClients
-@EnableHystrix
+//@EnableHystrix
 public class MoviceFeignApplication {
 
 	public static void main(String[] args) {
