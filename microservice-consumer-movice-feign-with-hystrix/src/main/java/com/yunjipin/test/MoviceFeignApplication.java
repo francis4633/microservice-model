00962bfea1e6f97f0e6931fec36458d5c9b58fa8
674145395@qq.com <674145395@qq.com>
2018-11-08 17:07:10 +0800

    @EnableFeignClients，feign中就已经包含了对hystrix的支持，故不需要引进hystrix的jar包，有@EnableFeignClients即可，但默认feign对hytrix的支持是关闭的。需要在yml文件中配置开启

--- microservice-consumer-movice-feign-with-hystrix/src/main/java/com/yunjipin/test/MoviceFeignApplication.java
+++ microservice-consumer-movice-feign-with-hystrix/src/main/java/com/yunjipin/test/MoviceFeignApplication.java
@@ -9,9 +9,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-//@SpringBootApplication
-@SpringCloudApplication
-//@EnableEurekaClient
+@SpringBootApplication
+//@SpringCloudApplication
+@EnableEurekaClient
 @EnableFeignClients
 //@EnableHystrix
 public class MoviceFeignApplication {
